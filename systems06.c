9312d1a59b52b53470d3ec64e0495b03564464b0
KenLi8504 <kli22@stuy.edu>
2021-10-04 02:54:39 -0400

    Completed everything with *

--- systems06.c
+++ systems06.c
@@ -10,6 +10,23 @@ int main(){
   printf("[");
   for(int loop = 0; loop < 9; loop++)
    printf("%d, ", original[loop]);
-   printf("%d] \n",original[10]);
+   printf("%d] \n",original[9]);
+
+  int new[10];
+
+  int *originalPointer = original;
+  int *newPointer = new;
+
+  for (int index = 0; index < 10; index++){
+    *(newPointer + 9 - index) = original[index];
+    int test = original[index];
+    int newTest = new[9-index];
+  }
+
+  printf("[");
+  for(int loop = 0; loop < 9; loop++)
+   printf("%d, ", new[loop]);
+   printf("%d] \n",new[9]);
+
   return 0;
 }
